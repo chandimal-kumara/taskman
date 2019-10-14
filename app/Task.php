674c4f58911b82a028938ebc163d7e8d648f7b09
Chandimal Kumara <kumara.8375@gmail.com>
2019-10-14 15:36:12 +0530

    yyY

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['title', 'description', 'content', 'created', 'taskStatus', 'assign', 'workStatus'];
+    protected $fillable = ['task_code', 'title', 'type', 'priority', 'description', 'content', 'estimated_hours', 'created', 'assign', 'task_status'];
 }
